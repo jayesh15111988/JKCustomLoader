e2d3f71c08edbba6f7350abf409abb984987b0d6
Jayesh Kawli <j.kawli@gmail.com>
2015-03-21 20:38:24 -0400

    Refined demo by adding flag for completed animations

--- JKCustomLoader/ViewController.m
+++ JKCustomLoader/ViewController.m
@@ -12,49 +12,62 @@
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UIImageView *testImageView;
 @property (assign) MaskShapeType selectedMaskShapeType;
+@property (weak, nonatomic) IBOutlet UILabel *animationCompleteLabel;
+@property (assign) BOOL isAnimationComplete;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.isAnimationComplete = YES;
 }
 
 -(void)loadImageWithSelectedMask {
-    JKCustomLoader* loader = [[JKCustomLoader alloc] initWithInputView:self.testImageView andAnimationType:self.selectedMaskShapeType];
-    loader.maskImage = [UIImage imageNamed:@"donald.png"];
-    loader.numberOfSidesForStar = 5;
-    loader.pointinessForStarCorners = 2;
-    [loader loadViewWithPartialCompletionBlock:^(CGFloat partialCompletionPercentage) {
-        NSLog(@"Percentage Completed %f", partialCompletionPercentage);
-    } andCompletionBlock:^{
-        NSLog(@"Image Loading Completed");
-    }];
+    if(self.isAnimationComplete) {
+        self.animationCompleteLabel.text = @"Working on Animation......";
+        JKCustomLoader* loader = [[JKCustomLoader alloc] initWithInputView:self.testImageView andAnimationType:self.selectedMaskShapeType];
+        loader.maskImage = [UIImage imageNamed:@"donald.png"];
+        loader.numberOfSidesForStar = 5;
+        loader.pointinessForStarCorners = 2;
+        [loader loadViewWithPartialCompletionBlock:^(CGFloat partialCompletionPercentage) {
+            NSLog(@"Percentage Completed %f", partialCompletionPercentage);
+        } andCompletionBlock:^{
+            self.animationCompleteLabel.text = @"Complete";
+            self.isAnimationComplete = YES;
+            NSLog(@"Image Loading Completed");
+        }];
+    }
 }
 
 - (IBAction)loadRectangleAnimation:(id)sender {
     self.selectedMaskShapeType = MaskShapeTypeRectangle;
     [self loadImageWithSelectedMask];
+    self.isAnimationComplete = NO;
 }
 
 - (IBAction)loadCircleAnimation:(id)sender {
     self.selectedMaskShapeType = MaskShapeTypeCircle;
     [self loadImageWithSelectedMask];
+    self.isAnimationComplete = NO;
 }
 
 - (IBAction)loadTriangleAnimation:(id)sender {
     self.selectedMaskShapeType = MaskShapeTypeTriangle;
     [self loadImageWithSelectedMask];
+    self.isAnimationComplete = NO;
 }
 
 - (IBAction)loadPolygonAnimation:(id)sender {
     self.selectedMaskShapeType = MaskShapeTypeStar;
     [self loadImageWithSelectedMask];
+    self.isAnimationComplete = NO;
 }
 
 - (IBAction)loadImageAnimation:(id)sender {
     self.selectedMaskShapeType = MaskShapeTypeAlphaImage;
     [self loadImageWithSelectedMask];
+    self.isAnimationComplete = NO;
 }
 
 @end
